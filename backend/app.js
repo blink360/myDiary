89d0992eca56105396c51359f5935a1fc5d1a148
Pranish Rajbhandari <rajbhandari.pranish@gmail.com>
2020-07-28 13:37:39 +0545

    added cors for backend

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,9 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+let createError = require("http-errors");
+let express = require("express");
+let path = require("path");
+let cookieParser = require("cookie-parser");
+let logger = require("morgan");
+let cors = require("cors");
 require("dotenv").config();
 
 var app = express();
@@ -12,11 +13,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use(cors());
 //import routers
 app.use("/user", require("./routes/user"));
 app.use("/entries", require("./routes/diaryEntry"));
 app.use("/diaryEntryAnalysis", require("./routes/sentimentAnalyzer"));
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
